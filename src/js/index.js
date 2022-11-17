decb8a9460740de19de284cf4f3b0d96764eaa8d
Lucas Vieira <lucasvieira@luttos.com>
2022-11-17 03:03:40 -0300

    color filter

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,14 @@
 const serverurl = process.env.SERVER_API;
-
 console.log("Dev m3", serverurl);
 
 let loadedProducts = []
 let renderedElements = []
-let filters = {
-  orderBy: '',
-  sizes: [],
-  colors: [],
-  price: []
-}
+let allFilters = []
 let filteredProducts = []
 
 const cardContainer = document.getElementById('cards-content');
 const loadMoreButton = document.getElementById('load_more');
+const allColorsButton = document.getElementById('all_colors');
 const orderByButton = document.getElementById('order_by')
 
 function priceFormat(price) {
@@ -23,7 +18,16 @@ function priceFormat(price) {
   }).format(price);
 }
 
-function renderCard({image, name, parcelamento, price}) {
+function displayProducts(products) {
+  const allCards = document.querySelectorAll('.card');
+  allCards.forEach(card => card.remove());
+
+  products.map(product => {
+    renderCard(product)
+  })
+}
+
+function renderCard({id, image, name, parcelamento, price}) {
   const cardElement = document.createElement('div');
   const buttonElement = document.createElement('a')
   const imgElement = document.createElement('img');
@@ -31,7 +35,6 @@ function renderCard({image, name, parcelamento, price}) {
   const priceElement = document.createElement('strong');
   const installmentElement = document.createElement('span');
 
-  
   cardElement.classList.add('card');
 
   imgElement.classList.add('card-image')
@@ -70,7 +73,9 @@ function loadMore() {
       renderedElements.push(loadedProducts[count]);
       renderCard(loadedProducts[count])
     }
-  } 
+  }
+
+  loadFiltered(allFilters);
 }
 
 function toggleOrderBy() {
@@ -96,34 +101,106 @@ function orderByOptions() {
   })
 }
 
-loadMoreButton.addEventListener('click', loadMore)
+allColorsButton.addEventListener('click', () => {
+  const allColors = document.querySelectorAll('.container.-hide');
+  allColorsButton.classList.add('-hide');
+  allColors.forEach(color => color.classList.remove('-hide'));
+})
+loadMoreButton.addEventListener('click', loadMore);
 
 orderByButton.addEventListener('click', () => {
   toggleOrderBy();
-})
+});
 
-function loadFiltered() {
+function allInput() {
+  const inputs = document.querySelectorAll('input[type="checkbox"]');
+
+  inputs.forEach(input => {
+    setInputsEventListener(input)
+  })
+}
+
+function setInputsEventListener(input) {
+  input.addEventListener('click', allInputChecked)
+}
+
+function allInputChecked() {
+  const inputs = document.querySelectorAll('input[type="checkbox"]');
+  const filters = [];
+
+  inputs.forEach(input => {
+    if (input.checked) filters.push(input.dataset)
+  })
+
+  allFilters = filters;
+  loadFiltered(filters)
+}
+
+function loadFiltered(filters) {
+  const colors = [];
+  const sizes = [];
+
+  filters.map((filter) => {
+    if (filter.color) {
+      colors.push(filter.color)
+    }
+
+    // if (filter.size) {
+    //   if (filter.value) {
+    //     // console.log(filter.size, filter.value);
+    //     const sizeFilter = {
+    //       ...sizes,
+    //       [filter.size]: filter.value 
+    //     }
+    //     sizes.push(sizeFilter)
+    //     console.log(sizes);
+    //   }
+    // }
+  })
+
+  const productsFiltered = [];
+  
+  colors.map(color => {
+    const filteredColors = loadedProducts.filter(product => product.color.toUpperCase() == color.toUpperCase());
+    productsFiltered.push(...filteredColors);
+  })
+  
+  // if (!productsFiltered.length == 0) {
+  // } else {
+  //   loadedProducts.forEach(card => renderCard(card));
+  // }
+  
+  if (productsFiltered.length == 0) {
+    displayProducts(loadedProducts);
+  } else {
+    displayProducts(productsFiltered);
+  }
+  
   // Aqui eu vou percorrer todo o filters e a partir do loadedProducts fazer um filter e renderizar tudo novamente
   // const filteredProducts = loadedProducts.filter(filters...)
 
   // filteredProducts.map(product, renderCard(product))
-  console.log('Fazendo todo processo de filtragem salvando nos filtros', filters);
+  // console.log('Fazendo todo processo de filtragem salvando nos filtros', filters);
 }
+
 
 function loadProducts() {
   fetch('http://localhost:5000/products')
   .then(data => {
     return data.json()
   })
-  .then(products => {
-    loadedProducts = products;
+  .then(allProducts => {
+    loadedProducts = allProducts;
+    const products = []
     for (let count = 0; count < 9; count++) {
-      renderedElements.push(products[count]);
-      renderCard(products[count])
+      renderedElements.push(allProducts[count]);
+      products.push(allProducts[count])
     }
+    displayProducts(products);
   })
 }
 
 loadProducts();
 loadMore();
+allInput()
 orderByOptions()
